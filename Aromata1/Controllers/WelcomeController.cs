4b0fc1c833295fe323a48fc13d840fbb0b135144
Omar-A-96 <o.abdelkader1996@gmail.com>
2025-03-28 23:34:05 +0200

    Add Welcome Controller

---
+++ Aromata1/Controllers/WelcomeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Aromata1.Controllers
+{
+    public class WelcomeController : Controller
+    {
+        public IActionResult Home()
+        {
+            string name = "Omar Abdelkader Mohamed";
+            int age = 28;
+            double salary = 10000.5;
+
+            var person = new { name, age, salary };
+
+            return View(model: person);
+        }
+    }
+}
